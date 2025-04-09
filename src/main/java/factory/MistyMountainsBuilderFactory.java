e8cb2b3477e4d9731aeee2835fc6c8445f753799
Shirouky <83469591+Shirouky@users.noreply.github.com>
2025-04-09 12:54:51 +0300

    Edit

--- src/main/java/factory/MistyMountainsBuilderFactory.java
+++ src/main/java/factory/MistyMountainsBuilderFactory.java
@@ -1,11 +1,17 @@
 package factory;
 
+import com.github.javafaker.Faker;
 import gear.factory.MistyMountainsGearFactory;
+import ork.builder.MistyMountainsBuilder;
 import ork.builder.OrkBuilder;
 
 public class MistyMountainsBuilderFactory implements OrkBuilderFactory {
     @Override
     public OrkBuilder createOrkBuilder(String name) {
-        return new OrkBuilder(new MistyMountainsGearFactory(), name);
+        Faker faker = new Faker();
+        if (name.isBlank()) {
+            name = faker.lordOfTheRings().character();
+        }
+        return new MistyMountainsBuilder(new MistyMountainsGearFactory(), name);
     }
 }
